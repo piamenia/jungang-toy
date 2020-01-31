2e48d489e848f2abf09fb58bcf5bb171297df893
JaehoonKim <pepper.hoon@kakaocorp.com>
2020-01-31 11:14:44 +0900

    init commit

---
+++ src/test/java/io/dktechin/eapproval/EapprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dktechin.eapproval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EapprovalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
